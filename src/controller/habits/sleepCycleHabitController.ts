47349662aecaf56de25d8542e8bf780a6fa0764d
Prabhashperera <prabashmperera01@gmail.com>
2025-11-19 20:12:13 +0530

    Sleep Log Save Complete

--- src/controller/habits/sleepCycleHabitController.ts
+++ src/controller/habits/sleepCycleHabitController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import GenAi from "../../config/geminiConfig";
+import SleepCycleModel from "../../model/habits/sleepCycleHabit";
 
 interface Part {
     text?: string;       // the main text
@@ -32,10 +33,12 @@ function calculatePoints(isSleptInWindow: boolean, isWokeInWindow: boolean) {
 }
 
 async function geminiDailyAdvice(sleptAt: string, wokeAt: string, userName: string) {
-    const prompt = `The ${userName} slept at ${sleptAt} and woke at ${wokeAt}.
-    but i given sleep hour is arround ${SLEEP_START} - ${SLEEP_END},
-    and the wakeup time i gven to him is ${WAKE_START} - ${WAKE_END}.
-    Give a friendly,funny,short advice for improving him to sleep habits (dont give introductions, just give the advice).`
+    const prompt = `Hey ${userName}! I see you slept at ${sleptAt} and woke up at ${wokeAt}. 
+    Your ideal sleep window is around ${SLEEP_START} - ${SLEEP_END}, 
+    and your ideal wake-up window is ${WAKE_START} - ${WAKE_END}. 
+    Give me a short, friendly, and funny tip to help me improve my sleep habits. 
+    Keep it casual, encouraging, and the last give a tip or tips according to the situation 
+    and skip any introductions (use simple english words) — just the advice!`;
 
     // const models = await GenAi.models.list();
     // console.log(models);
@@ -46,8 +49,7 @@ async function geminiDailyAdvice(sleptAt: string, wokeAt: string, userName: stri
 
     const result = await model.generateContent(prompt);
     console.log(result.response.text());
-    return result;
-
+    return result.response.text();
 
 }
 
@@ -59,7 +61,7 @@ export const saveDailyLog = async (req: Request, res: Response) => {
 
 
         const currentDate = new Date("2025-01-01").getFullYear(); // Current Date
-        const userId = req.user //Current User ID
+        const userId = req.user?.userId //Current User ID
         const { sleptAt, wokeAt } = req.body //All The Request Data
         const isSleptInWindow = isInTimeWindow(sleptAt, SLEEP_START, SLEEP_END) // isSleptInWindow
         const isWokeInWindow = isInTimeWindow(wokeAt, WAKE_START, WAKE_END) // isWokeInWindow
@@ -69,13 +71,22 @@ export const saveDailyLog = async (req: Request, res: Response) => {
         console.log(currentDate, userId, sleptAt, wokeAt);
         console.log(isSleptInWindow, isWokeInWindow, pointsAwarded);
         console.log("Advice : " + dailyAdvice);
-        res.status(200).json({
-            sleptAt,
-            wokeAt,
-            isSleptInWindow,
-            isWokeInWindow,
-            pointsAwarded,
-            advice: dailyAdvice
+
+        const newSleepLog = new SleepCycleModel({
+            userId: userId,
+            date: currentDate,
+            sleptAt: sleptAt,
+            wokeAt: wokeAt,
+            sleptInWindow: isSleptInWindow,
+            wokeInWindow: isWokeInWindow,
+            pointsAwarded: pointsAwarded,
+            aiAdvice: dailyAdvice
+        });
+
+        const savedDailyLog = await newSleepLog.save()
+        res.status(201).json({
+            message: "Log Saved Success",
+            data: { savedDailyLog, dailyAdvice }
         })
 
 
