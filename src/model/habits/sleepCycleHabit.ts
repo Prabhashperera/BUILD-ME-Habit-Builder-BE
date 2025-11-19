a671c9fc579bc3e990969b503ae21c82fef234ed
Prabhashperera <prabashmperera01@gmail.com>
2025-11-19 13:50:22 +0530

    Sleep Habbit Model

---
+++ src/model/habits/sleepCycleHabit.ts
@@ -0,0 +1,38 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface ISleepCycle extends Document {
+    userId: mongoose.Schema.Types.ObjectId,
+    dailyLogs: {
+        date: Date,
+        sleptAt: string,
+        wokeAt: string,
+        sleptInWindow: boolean,
+        wokeInWindow: boolean,
+        pointsAwarded: number,
+        aiAdvice?: string
+    }[];
+}
+
+// Daily Logs Seperation
+const dailyLogsSchema = new mongoose.Schema({
+    date: { type: Date, required: true },
+    sleptAt: { type: String },
+    wokeAt: { type: String },
+    sleptInWindow: { type: Boolean },
+    wokeInWindow: { type: Boolean },
+    pointsAwarded: { type: Number },
+    aiAdvice: { type: String },
+}, { _id: false })
+
+// Sleep Cycle With Daly logs Intergration
+const sleepCycleHabit: Schema<ISleepCycle> = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    dailyLogs: [dailyLogsSchema]
+}, { timestamps: true });
+
+const SleepCycleModel = mongoose.model<ISleepCycle>("SleepCycleModel", sleepCycleHabit)
+export default SleepCycleModel
